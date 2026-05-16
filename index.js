f8767ea2e68fb13546bab31710c9b24d20d6b432
Geraldine Tafur <geraldine.tafurg@gmail.com>
2026-05-16 16:30:51 -0500

    Fix event listener setup for emotion radios and modal functionality

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ const memeModalInner = document.getElementById('meme-modal-inner')
 const memeModal = document.getElementById('meme-modal')
 const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
 
+motionRadios.addEventListener('change', highlightCheckedOption)
+
+memeModalCloseBtn.addEventListener('click', closeModal)
+
+getImageBtn.addEventListener('click', renderCat)
+
+function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+    for (let radio of radios){
+        radio.classList.remove('highlight')
+    }
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+function closeModal(){
+    memeModal.style.display = 'none'
+}
+
 function getEmotionsArray(cats){
     const emotionsArray = []
     for (let cat of cats){
