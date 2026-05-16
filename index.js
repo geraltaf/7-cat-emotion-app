a71146233a5d24038370cb4fb2d495e2c423ceef
Geraldine Tafur <geraldine.tafurg@gmail.com>
2026-05-16 16:29:28 -0500

    Add modal and button element references for improved UI interaction

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { catsData } from '/data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
 
 function getEmotionsArray(cats){
     const emotionsArray = []
